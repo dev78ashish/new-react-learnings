de71583db08a98a4f3d8e509b31caf02a479a431
dev78ashish <dev78ashish@gmail.com>
2025-05-08 10:55:41 +0530

    responsiveness fixes

--- starcheck-clone/src/components/Home.jsx
+++ starcheck-clone/src/components/Home.jsx
@@ -3,7 +3,7 @@ import Main from './Main/Main'
 
 const Home = () => {
   return (
-    <div className='bg-[#D2C9D5] py-10 px-20'>
+    <div className='bg-[#D2C9D5] px-6 py-6 md:px-10 md:py-8 lg:px-20 lg:py-10'>
       <Main />
     </div>
   )
--- starcheck-clone/src/components/Main/components/EmailSignup.jsx
+++ starcheck-clone/src/components/Main/components/EmailSignup.jsx
@@ -11,8 +11,8 @@ export default function EmailSignup() {
 
   return (
     <div className="bg-[#7E6AF0] my-30 rounded-4xl p-12 text-center w-full">
-      <h2 className="text-white text-6xl font-bold mb-6">
-        Leave your email and 
+      <h2 className="text-white text-3xl md:text-5xl lg:text-6xl font-bold mb-6">
+        Leave your email and
         <br />
         get a free check!
       </h2>
@@ -24,19 +24,21 @@ export default function EmailSignup() {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             placeholder="Your email"
-            className="w-full rounded-full px-6 pr-36 h-16 text-2xl text-gray-700 bg-amber-50 focus:outline-none"
+            className="w-full rounded-full px-6 pr-36 h-16 md:text-xl lg:text-2xl text-gray-700 bg-amber-50 focus:outline-none"
             required
           />
-          <button 
+          <button
             type="submit"
-            className="absolute top-1/2 right-2 -translate-y-1/2 bg-gray-800 text-white font-medium px-5 py-2 h-[80%] rounded-full text-lg"
+            className="absolute top-1/2 right-2 -translate-y-1/2 bg-gray-800 text-white font-medium px-5 py-2 h-[80%] rounded-full md:text-xl lg:text-lg"
           >
             Subscribe
           </button>
         </div>
       </form>
 
-      <p className="text-white text-2xl opacity-55">No spam, promised!</p>
+      <p className="text-white text-base md:text-xl lg:text-2xl opacity-55">
+        No spam, promised!
+      </p>
     </div>
   );
 }
